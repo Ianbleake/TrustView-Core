5cb5ace7be7cff609d97fd9ed00994b16d84fe04
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-23 16:29:20 -0600

    fix; update widget condfig service

--- src/services/storeService.js
+++ src/services/storeService.js
@@ -4,7 +4,7 @@ export async function updateWidgetService(store_id, widget_config) {
 
   const { data, error } = await supabase
     .from("stores")
-    .update("widget_config", widget_config)
+    .update({ widget_config })
     .eq("id", store_id)
     .select("id, widget_config")
     .single();
