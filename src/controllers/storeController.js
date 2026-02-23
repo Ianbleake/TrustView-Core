156fb28562e270bb393e5b0e439f7ebe7e62ad01
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-23 16:23:07 -0600

    fix: widget config names

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -3,9 +3,9 @@ import { successResponse } from "../utils/response.js";
 
 export async function updateWidget(req, res, next) {
   try {
-    const { store_id, widget_settings } = req.body;
+    const { store_id, widget_config } = req.body;
 
-    const updatedStore = await updateWidgetService(store_id, widget_settings);
+    const updatedStore = await updateWidgetService(store_id, widget_config);
 
     successResponse(res, {
       status: 200,
--- src/services/storeService.js
+++ src/services/storeService.js
@@ -1,10 +1,10 @@
 import { supabase } from "../config/supabase.js";
 
-export async function updateWidgetService(store_id, widget_settings) {
+export async function updateWidgetService(store_id, widget_config) {
 
   const { data, error } = await supabase
     .from("stores")
-    .update("widget_config", widget_settings)
+    .update("widget_config", widget_config)
     .eq("id", store_id)
     .select("id, widget_config")
     .single();
